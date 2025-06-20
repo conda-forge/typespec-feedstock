fced31a64216f3322f341b2367d08f4b6e47ae29
Daniel Nachun <daniel.nachun@gmail.com>
2025-06-20 12:59:43 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     ${SRC_DIR}/typespec-compiler-${PKG_VERSION}.tgz
@@ -13,10 +14,23 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/tsp << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/@typespec/compiler/cmd/tsp.js "\$@"
+EOF
+chmod +x ${PREFIX}/bin/tsp
+
+tee ${PREFIX}/bin/tsp-server << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/@typespec/compiler/cmd/tsp-server.js "\$@"
+EOF
+chmod +x ${PREFIX}/bin/tsp-server
+
 tee ${PREFIX}/bin/tsp.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\tsp %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\@typespec\compiler\cmd\tsp.js %*
 EOF
 
 tee ${PREFIX}/bin/tsp-server.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\tsp-server %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\@typespec\compiler\cmd\tsp-server.js %*
 EOF
